8db4e36437801eaf5fb3ecdaa158e5377ced0586
Nikolay <nikolayrudakov@mail.ru>
2023-02-13 17:10:56 +0600

    final commit

--- Program.cs
+++ Program.cs
@@ -1,22 +1,5 @@
 ﻿Console.Clear();
 
-// string word = "Hello world";
-// string myName = "Nikolay";
-// Console.WriteLine(word.Length);
-// string[] arrayOfStrings = new string[2];
-// arrayOfStrings[0] = word;
-// arrayOfStrings[1] = myName;
-
-// void showArray(string[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-// }
-//string.Join(", ", array); - Show Array;
-
-
 string[] stringArray1 = { "hello", "2", "world", ":-)" };
 string[] stringArray2 = { "1234", "1567", "-2", "computer science" };
 string[] stringArray3 = { "Russia", "Denmark", "Kazan" };
@@ -50,3 +33,20 @@ Console.Write($"Initial array: [{string.Join(", ", stringArray1)}]");
 Console.WriteLine(' ');
 Console.Write("Result: ");
 showFilteredArray(stringArray1);
+
+// string word = "Hello world";
+// string myName = "Nikolay";
+// Console.WriteLine(word.Length);
+// string[] arrayOfStrings = new string[2];
+// arrayOfStrings[0] = word;
+// arrayOfStrings[1] = myName;
+
+// void showArray(string[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+// }
+//string.Join(", ", array); - Show Array;
+
